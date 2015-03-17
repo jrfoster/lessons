a53889df45f0d78749aabf7c35ce29ed1e91b659
Jason Foster <jason.foster@orionhealth.com>
2015-03-17 14:43:37 -0700

    Fixing unit test

--- src/test/java/lessons/TestLED.java
+++ src/test/java/lessons/TestLED.java
@@ -1,11 +1,11 @@
 package lessons;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail; 
 
 import java.io.ByteArrayOutputStream;
-import java.util.BitSet;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -13,45 +13,38 @@ public class TestLED {
 
 	@Test
 	public void testDisplay() {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		LED led = new LED(baos);
+		ByteArrayOutputStream baos = null;
 		
-		assertTrue(led.display('0'));
-		System.out.println(baos);
-		//assertEquals(led.toString(), BitSet.valueOf(new byte[] {119}).toString());
-		assertTrue(led.display('1'));
-		System.out.println(baos);
-		//assertEquals(led.toString(), BitSet.valueOf(new byte[] {36}).toString());
-		assertTrue(led.display('2'));
-		System.out.println(baos);
-		//assertEquals(led.toString(), BitSet.valueOf(new byte[] {93}).toString());
-		assertTrue(led.display('3'));
-		System.out.println(baos);
-		//assertEquals(led.toString(), BitSet.valueOf(new byte[] {109}).toString());
-		assertTrue(led.display('4'));
-		System.out.println(baos);
-		//assertEquals(led.toString(), BitSet.valueOf(new byte[] {46}).toString());
-		assertTrue(led.display('5'));
-		System.out.println(baos);
-		//assertEquals(led.toString(), BitSet.valueOf(new byte[] {107}).toString());
-		assertTrue(led.display('6'));
-		System.out.println(baos);
-		//assertEquals(led.toString(), BitSet.valueOf(new byte[] {122}).toString());
-		assertTrue(led.display('7'));
-		System.out.println(baos);
-		//assertEquals(led.toString(), BitSet.valueOf(new byte[] {37}).toString());
-		assertTrue(led.display('8'));
-		System.out.println(baos);
-		//assertEquals(led.toString(), BitSet.valueOf(new byte[] {127}).toString());
-		assertTrue(led.display('9'));
-		System.out.println(baos);
-		//assertEquals(led.toString(), BitSet.valueOf(new byte[] {111}).toString());
-		
-		for (char c = 0; c < 256; c++) {
-			if (c >= 48 && c <= 57)
-			   assertTrue(led.display(c));
-			else
-			   assertFalse(led.display(c));
+		try {
+			baos = new ByteArrayOutputStream();
+			LED led = new LED(baos);
+			
+			assertTrue(led.display('0'));
+			assertTrue(led.display('1'));
+			assertTrue(led.display('2'));
+			assertTrue(led.display('3'));
+			assertTrue(led.display('4'));
+			assertTrue(led.display('5'));
+			assertTrue(led.display('6'));
+			assertTrue(led.display('7'));
+	 		assertTrue(led.display('8'));
+			assertTrue(led.display('9'));
+			assertTrue(Arrays.equals(baos.toByteArray(), 
+					new byte[] {119,36,93,109,46,107,122,37,127,111}));
+			
+			
+			for (char c = 0; c < 256; c++) {
+				if (c >= 48 && c <= 57)
+				   assertTrue(led.display(c));
+				else
+				   assertFalse(led.display(c));
+			}
+		} catch (Exception ex) {
+			fail(ex.getMessage());
+		} finally {
+			if (baos != null) {
+				try { baos.close(); } catch (Exception ignored) {}				
+			}
 		}
 	}
 }
