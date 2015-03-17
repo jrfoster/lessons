44b8371a228d14f88248d3e5237e7a9458ace940
Jason Foster <jason.foster@orionhealth.com>
2015-03-17 15:20:53 -0700

    Basic formatting

--- src/main/java/lessons/LED.java
+++ src/main/java/lessons/LED.java
@@ -1,15 +1,15 @@
+
 package lessons;
 
 import java.nio.ByteBuffer;
 import java.util.BitSet;
 
 /**
- * Represents the lighted portion of an LED. We use seven positions in an array
- * to represent each of the "pixels" of our display. Each of the positions of
- * the LED corresponds to a specific cell in our array and for the purposes of
- * display, its either on or off. We start numbering at the top, and work our
- * way, top-down, left to right. Using this approach, the representation of the
- * numbers 0-9 are the following:<br>
+ * Represents the lighted portion of an LED. We use seven positions in an array to represent each of
+ * the "pixels" of our display. Each of the positions of the LED corresponds to a specific cell in
+ * our array and for the purposes of display, its either on or off. We start numbering at the top,
+ * and work our way, top-down, left to right. Using this approach, the representation of the numbers
+ * 0-9 are the following:<br>
  * <br>
  * <table>
  * <tr>
@@ -68,170 +68,171 @@ import java.util.BitSet;
  */
 public class LED {
 
-	private final BitSet pixels = new BitSet(7);
-	private final ByteBuffer deviceBuffer;
+   private final BitSet pixels = new BitSet(7);
+   private final ByteBuffer deviceBuffer;
 
-	public LED(ByteBuffer outputBuffer) {
-		if (outputBuffer == null)
-			throw new IllegalArgumentException("outputBuffer cannot be null");
+   public LED(ByteBuffer outputBuffer) {
+      if (outputBuffer == null)
+         throw new IllegalArgumentException("outputBuffer cannot be null");
 
-		deviceBuffer = outputBuffer;
-	}
+      deviceBuffer = outputBuffer;
+   }
 
-	public boolean display(char value) {
-		switch (value) {
-		case '0':
-			return set0();
-		case '1':
-			return set1();
-		case '2':
-			return set2();
-		case '3':
-			return set3();
-		case '4':
-			return set4();
-		case '5':
-			return set5();
-		case '6':
-			return set6();
-		case '7':
-			return set7();
-		case '8':
-			return set8();
-		case '9':
-			return set9();
-		default:
-			return false;
-		}
-	}
+   public boolean display(char value) {
+      switch (value) {
+      case '0':
+         return set0();
+      case '1':
+         return set1();
+      case '2':
+         return set2();
+      case '3':
+         return set3();
+      case '4':
+         return set4();
+      case '5':
+         return set5();
+      case '6':
+         return set6();
+      case '7':
+         return set7();
+      case '8':
+         return set8();
+      case '9':
+         return set9();
+      default:
+         return false;
+      }
+   }
 
-	private boolean set0() {
-		// 0 1110111
-		pixels.set(0);
-		pixels.set(1);
-		pixels.set(2);
-		pixels.clear(3);
-		pixels.set(4);
-		pixels.set(5);
-		pixels.set(6);
-		return deviceOut();
-	}
+   private boolean set0() {
+      // 0 1110111
+      pixels.set(0);
+      pixels.set(1);
+      pixels.set(2);
+      pixels.clear(3);
+      pixels.set(4);
+      pixels.set(5);
+      pixels.set(6);
+      return deviceOut();
+   }
 
-	private boolean set1() {
-		// 1 0010010
-		pixels.clear(0);
-		pixels.clear(1);
-		pixels.set(2);
-		pixels.clear(3);
-		pixels.clear(4);
-		pixels.set(5);
-		pixels.clear(6);
-		return deviceOut();
-	}
+   private boolean set1() {
+      // 1 0010010
+      pixels.clear(0);
+      pixels.clear(1);
+      pixels.set(2);
+      pixels.clear(3);
+      pixels.clear(4);
+      pixels.set(5);
+      pixels.clear(6);
+      return deviceOut();
+   }
 
-	private boolean set2() {
-		// 2 1011101
-		pixels.set(0);
-		pixels.clear(1);
-		pixels.set(2);
-		pixels.set(3);
-		pixels.set(4);
-		pixels.clear(5);
-		pixels.set(6);
-		return deviceOut();
-	}
+   private boolean set2() {
+      // 2 1011101
+      pixels.set(0);
+      pixels.clear(1);
+      pixels.set(2);
+      pixels.set(3);
+      pixels.set(4);
+      pixels.clear(5);
+      pixels.set(6);
+      return deviceOut();
+   }
 
-	private boolean set3() {
-		// 3 1011011
-		pixels.set(0);
-		pixels.clear(1);
-		pixels.set(2);
-		pixels.set(3);
-		pixels.clear(4);
-		pixels.set(5);
-		pixels.set(6);
-		return deviceOut();
-	}
+   private boolean set3() {
+      // 3 1011011
+      pixels.set(0);
+      pixels.clear(1);
+      pixels.set(2);
+      pixels.set(3);
+      pixels.clear(4);
+      pixels.set(5);
+      pixels.set(6);
+      return deviceOut();
+   }
 
-	private boolean set4() {
-		// 4 0111010
-		pixels.clear(0);
-		pixels.set(1);
-		pixels.set(2);
-		pixels.set(3);
-		pixels.clear(4);
-		pixels.set(5);
-		pixels.clear(6);
-		return deviceOut();
-	}
+   private boolean set4() {
+      // 4 0111010
+      pixels.clear(0);
+      pixels.set(1);
+      pixels.set(2);
+      pixels.set(3);
+      pixels.clear(4);
+      pixels.set(5);
+      pixels.clear(6);
+      return deviceOut();
+   }
 
-	private boolean set5() {
-		// 5 1101011
-		pixels.set(0);
-		pixels.set(1);
-		pixels.clear(2);
-		pixels.set(3);
-		pixels.clear(4);
-		pixels.set(5);
-		pixels.set(6);
-		return deviceOut();
-	}
+   private boolean set5() {
+      // 5 1101011
+      pixels.set(0);
+      pixels.set(1);
+      pixels.clear(2);
+      pixels.set(3);
+      pixels.clear(4);
+      pixels.set(5);
+      pixels.set(6);
+      return deviceOut();
+   }
 
-	private boolean set6() {
-		// 6 0101111
-		pixels.clear(0);
-		pixels.set(1);
-		pixels.clear(2);
-		pixels.set(3);
-		pixels.set(4);
-		pixels.set(5);
-		pixels.set(6);
-		return deviceOut();
-	}
+   private boolean set6() {
+      // 6 0101111
+      pixels.clear(0);
+      pixels.set(1);
+      pixels.clear(2);
+      pixels.set(3);
+      pixels.set(4);
+      pixels.set(5);
+      pixels.set(6);
+      return deviceOut();
+   }
 
-	private boolean set7() {
-		// 7 1010010
-		pixels.set(0);
-		pixels.clear(1);
-		pixels.set(2);
-		pixels.clear(3);
-		pixels.clear(4);
-		pixels.set(5);
-		pixels.clear(6);
-		return deviceOut();
-	}
+   private boolean set7() {
+      // 7 1010010
+      pixels.set(0);
+      pixels.clear(1);
+      pixels.set(2);
+      pixels.clear(3);
+      pixels.clear(4);
+      pixels.set(5);
+      pixels.clear(6);
+      return deviceOut();
+   }
 
-	private boolean set8() {
-		// 8 1111111
-		pixels.set(0);
-		pixels.set(1);
-		pixels.set(2);
-		pixels.set(3);
-		pixels.set(4);
-		pixels.set(5);
-		pixels.set(6);
-		return deviceOut();
-	}
+   private boolean set8() {
+      // 8 1111111
+      pixels.set(0);
+      pixels.set(1);
+      pixels.set(2);
+      pixels.set(3);
+      pixels.set(4);
+      pixels.set(5);
+      pixels.set(6);
+      return deviceOut();
+   }
 
-	private boolean set9() {
-		// 9 1111011
-		pixels.set(0);
-		pixels.set(1);
-		pixels.set(2);
-		pixels.set(3);
-		pixels.clear(4);
-		pixels.set(5);
-		pixels.set(6);
-		return deviceOut();
-	}
+   private boolean set9() {
+      // 9 1111011
+      pixels.set(0);
+      pixels.set(1);
+      pixels.set(2);
+      pixels.set(3);
+      pixels.clear(4);
+      pixels.set(5);
+      pixels.set(6);
+      return deviceOut();
+   }
 
-	private boolean deviceOut() {
-		try {
-			deviceBuffer.clear();
-			deviceBuffer.put(pixels.toByteArray());
-			return true;
-		} catch (Exception ex) {
-			return false;
-		}
-	}
+   private boolean deviceOut() {
+      try {
+         deviceBuffer.clear();
+         deviceBuffer.put(pixels.toByteArray());
+         return true;
+      }
+      catch (Exception ex) {
+         return false;
+      }
+   }
 }
--- src/test/java/lessons/TestLED.java
+++ src/test/java/lessons/TestLED.java
@@ -1,3 +1,4 @@
+
 package lessons;
 
 import static org.junit.Assert.assertFalse;
@@ -10,41 +11,42 @@ import org.junit.Test;
 
 public class TestLED {
 
-	@Test
-	public void testDisplay() {
-		try {
-			ByteBuffer buff = ByteBuffer.allocate(1);
-			LED led = new LED(buff);
+   @Test
+   public void testDisplay() {
+      try {
+         ByteBuffer buff = ByteBuffer.allocate(1);
+         LED led = new LED(buff);
 
-			assertTrue(led.display('0'));
-			assertTrue((byte) 119 == buff.get(0));
-			assertTrue(led.display('1'));
-			assertTrue((byte) 36 == buff.get(0));
-			assertTrue(led.display('2'));
-			assertTrue((byte) 93 == buff.get(0));
-			assertTrue(led.display('3'));
-			assertTrue((byte) 109 == buff.get(0));
-			assertTrue(led.display('4'));
-			assertTrue((byte) 46 == buff.get(0));
-			assertTrue(led.display('5'));
-			assertTrue((byte) 107 == buff.get(0));
-			assertTrue(led.display('6'));
-			assertTrue((byte) 122 == buff.get(0));
-			assertTrue(led.display('7'));
-			assertTrue((byte) 37 == buff.get(0));
-			assertTrue(led.display('8'));
-			assertTrue((byte) 127 == buff.get(0));
-			assertTrue(led.display('9'));
-			assertTrue((byte) 111 == buff.get(0));
+         assertTrue(led.display('0'));
+         assertTrue((byte) 119 == buff.get(0));
+         assertTrue(led.display('1'));
+         assertTrue((byte) 36 == buff.get(0));
+         assertTrue(led.display('2'));
+         assertTrue((byte) 93 == buff.get(0));
+         assertTrue(led.display('3'));
+         assertTrue((byte) 109 == buff.get(0));
+         assertTrue(led.display('4'));
+         assertTrue((byte) 46 == buff.get(0));
+         assertTrue(led.display('5'));
+         assertTrue((byte) 107 == buff.get(0));
+         assertTrue(led.display('6'));
+         assertTrue((byte) 122 == buff.get(0));
+         assertTrue(led.display('7'));
+         assertTrue((byte) 37 == buff.get(0));
+         assertTrue(led.display('8'));
+         assertTrue((byte) 127 == buff.get(0));
+         assertTrue(led.display('9'));
+         assertTrue((byte) 111 == buff.get(0));
 
-			for (char c = 0; c < 256; c++) {
-				if (c >= 48 && c <= 57)
-					assertTrue(led.display(c));
-				else
-					assertFalse(led.display(c));
-			}
-		} catch (Exception ex) {
-			fail(ex.getMessage());
-		}
-	}
+         for (char c = 0; c < 256; c++) {
+            if (c >= 48 && c <= 57)
+               assertTrue(led.display(c));
+            else
+               assertFalse(led.display(c));
+         }
+      }
+      catch (Exception ex) {
+         fail(ex.getMessage());
+      }
+   }
 }
